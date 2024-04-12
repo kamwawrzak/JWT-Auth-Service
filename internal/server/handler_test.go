a191f93b0645966a754a5819e6dc8a694c9c31b9
wawrzu <kamwawrzak@gmail.com>
2024-04-12 16:18:20 +0200

    login unit test

--- internal/server/handler_test.go
+++ internal/server/handler_test.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -53,6 +54,47 @@ func TestCreateUser(t *testing.T){
 	assert.Equal(t, http.StatusOK, rr.Code)
 	assert.Equal(t, expected, actual)
 	
+}
+
+func TestLoginSuccess(t *testing.T){
+	// arrange
+	handler, _, loginMock := getHandler()
+	user := prepareExampleUser()
+	input := &model.LoginInput{
+		Email: user.Email,
+		Password: user.Password,
+	}
+	expectedResult := &model.LoginResponse{
+		Jwt: "aaa.bbb.ccc",
+		ExpireAt: time.Now(),
+	}
+
+	loginMock.
+	On("Login", mock.Anything, mock.Anything, mock.Anything).
+	Return(expectedResult.Jwt, expectedResult.ExpireAt, nil)
+
+	jsonBody, err := json.Marshal(input)
+	require.NoError(t, err)
+
+	req, err := http.NewRequest("POST", "/login", bytes.NewBuffer(jsonBody))
+	if err != nil {
+		require.NoError(t, err)
+	}
+
+	rr := httptest.NewRecorder()
+	handlerWrapper := http.HandlerFunc(handler.Login)
+
+	// act
+	handlerWrapper.ServeHTTP(rr, req)
+
+	var actual *model.LoginResponse
+	err = json.NewDecoder(rr.Body).Decode(&actual)
+	require.NoError(t, err)
+
+	// assert
+	assert.Equal(t, http.StatusOK, rr.Code)
+	assert.Equal(t, expectedResult.Jwt, actual.Jwt)
+	assert.True(t, actual.ExpireAt.Before(time.Now().Add(time.Hour)))
 }
 
 func getHandler() (*AuthHandler, *mocks.MockSignupService, *mocks.MockLoginService) {
